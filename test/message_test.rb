912afd443a173cdfa5556305523b964b329f3204
Aaron Patterson <tenderlove@ruby-lang.org>
2024-02-05 15:54:05 -0800

    Implement decoding for one type

--- test/message_test.rb
+++ test/message_test.rb
@@ -3,23 +3,71 @@ require "proto/test/fixtures/test_pb"
 
 module ProtoBuff
   class TestMessage
-    def self.decode(bytes)
+    def self.decode(buff)
       obj = ::TestMessage.new
-      # decode bytes
+      # decode buff
       obj
     end
   end
 
   class Test1
-    def self.decode(bytes)
+    def self.decode(buff)
       obj = ::Test1.new
-      # decode bytes
+
+      # https://protobuf.dev/programming-guides/encoding/#structure
+      #
+      # The “tag” of a record is encoded as a varint formed from the field
+      # number and the wire type via the formula (field_number << 3) |
+      # wire_type. In other words, after decoding the varint representing a
+      # field, the low 3 bits tell us the wire type, and the rest of the
+      # integer tells us the field number.
+
+      buff_idx = 0
+
+      tag = buff.getbyte buff_idx
+
+      buff_idx += 1
+
+      # You take the last three bits to get the wire type (0)
+      wire_type = tag & 0x7
+
+      # and then right-shift by three to get the field number (1).
+      field_number = tag >> 3
+
+      value = nil
+
+      if wire_type == 0 # VARINT
+        value = 0
+        offset = 0
+        while true
+          byte = buff.getbyte buff_idx
+          part = byte & 0x7F # remove continuation bit
+
+          # We need to convert to big endian, so we'll "prepend"
+          value |= part << (7 * offset)
+
+          # Break if this byte doesn't have a continuation bit
+          break if byte < 0x80
+
+          buff_idx += 1
+          offset += 1
+        end
+
+        # Map the field number to a method we need to call
+        field_name = ::Test1.descriptor.to_a[field_number - 1].name
+
+        # send the method
+        obj.send("#{field_name}=", value)
+      else
+        raise "unknown wire type #{wire_type}"
+      end
+      # decode buff
       obj
     end
   end
 end
 
-class MessageTest < Minitest::Test
+ class MessageTest < Minitest::Test
   def test_decode_test_message
     data = "\n\vhello world\x10\xD2\t".b
     obj = ProtoBuff::TestMessage.decode data
@@ -30,15 +78,15 @@ class MessageTest < Minitest::Test
   end
 
   def test_decode_test1
-    data = "\b\xD2\t".b
+    data = "\b\x96\x01".b
     obj = ProtoBuff::Test1.decode data
-    assert_equal 1234, obj.a
+    assert_equal 150, obj.a
   end
 
   def test_encode_test1
-    data = "\b\xD2\t".b
+    data = "\b\x96\x01".b
     obj = Test1.new
-    obj.a = 1234
+    obj.a = 150
     assert_equal data, ProtoBuff::Test1.encode(obj)
   end
 end
