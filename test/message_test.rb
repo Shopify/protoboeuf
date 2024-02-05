38f3e47aad6bc5f783592f990d6ceab6561a7e82
Aaron Patterson <tenderlove@ruby-lang.org>
2024-02-05 14:59:32 -0800

    add some failing tests

--- test/message_test.rb
+++ test/message_test.rb
@@ -1,19 +1,44 @@
 require "minitest/autorun"
 require "proto/test/fixtures/test_pb"
 
-class MessageTest < Minitest::Test
-  def decode(bytes)
-    obj = TestMessage.new
-    # decode bytes
-    obj
+module ProtoBuff
+  class TestMessage
+    def self.decode(bytes)
+      obj = ::TestMessage.new
+      # decode bytes
+      obj
+    end
   end
 
-  def test_decoding
+  class Test1
+    def self.decode(bytes)
+      obj = ::Test1.new
+      # decode bytes
+      obj
+    end
+  end
+end
+
+class MessageTest < Minitest::Test
+  def test_decode_test_message
     data = "\n\vhello world\x10\xD2\t".b
-    obj = decode data
+    obj = ProtoBuff::TestMessage.decode data
 
     assert_equal "hello world", obj.id
     assert_equal 1234, obj.shop_id
     assert_equal false, obj.boolean
+  end
+
+  def test_decode_test1
+    data = "\b\xD2\t".b
+    obj = ProtoBuff::Test1.decode data
+    assert_equal 1234, obj.a
+  end
+
+  def test_encode_test1
+    data = "\b\xD2\t".b
+    obj = Test1.new
+    obj.a = 1234
+    assert_equal data, ProtoBuff::Test1.encode(obj)
   end
 end
