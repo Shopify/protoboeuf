e965be52602e1f5a82f62004e85e733496e22e57
Aaron Patterson <tenderlove@ruby-lang.org>
2024-02-07 15:56:48 -0800

    Add basic code generation
    This just walks the AST returned by the parser and generates code

--- test/helper.rb
+++ test/helper.rb
@@ -3,6 +3,7 @@ require "minitest/color"
 require "proto/test/fixtures/test_pb"
 require "protobuff/decoder"
 require "protobuff/parser"
+require "protobuff/codegen"
 
 module ProtoBuff
   class Test < Minitest::Test
