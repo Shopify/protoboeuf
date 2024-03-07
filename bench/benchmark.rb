dafa0a57be123d2a038b630bf647f884808e9b46
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2024-03-07 11:42:25 -0500

    Rename protobuff => protoboeuf in benchmark

--- bench/benchmark.rb
+++ bench/benchmark.rb
@@ -41,12 +41,12 @@ end
 
 Benchmark.ips { |x|
   x.report("decode upstream")  { Upstream::RedBlackNode.decode binary }
-  x.report("decode protobuff") { ProtoBuff::RedBlackNode.decode binary }
+  x.report("decode protoboeuf") { ProtoBuff::RedBlackNode.decode binary }
   x.compare!
 }
 
 Benchmark.ips { |x|
   x.report("decode and read upstream")  { walk Upstream::RedBlackNode.decode binary }
-  x.report("decode and read protobuff") { walk ProtoBuff::RedBlackNode.decode binary }
+  x.report("decode and read protoboeuf") { walk ProtoBuff::RedBlackNode.decode binary }
   x.compare!
 }
