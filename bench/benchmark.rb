a9fdd0402f62a9fc04f7492534868851b5235773
Colin Bendell <colin@bendell.ca>
2024-03-22 10:19:17 -0400

    For the benchmark, we want to exercise random values that are in the full range of the field type

--- bench/benchmark.rb
+++ bench/benchmark.rb
@@ -22,20 +22,51 @@ def pop_type_map(type_map, obj)
   end
 end
 
+@all_non_printable_characters = [
+  (127..160), # ascii non_printable
+  (0xD800..0xDFFF), # Surrogate pairs
+  (0xFDD0..0xFDEF), # noncharacters
+  [0xFFFE, 0xFFFF, 0x1FFFE, 0x1FFFF, 0x2FFFE, 0x2FFFF, 0x3FFFE, 0x3FFFF, 0x4FFFE, 0x4FFFF, 0x5FFFE, 0x5FFFF, 0x6FFFE, 0x6FFFF, 0x7FFFE, 0x7FFFF, 0x8FFFE, 0x8FFFF, 0x9FFFE, 0x9FFFF, 0xAFFFE, 0xAFFFF, 0xBFFFE, 0xBFFFF, 0xCFFFE, 0xCFFFF, 0xDFFFE, 0xDFFFF, 0xEFFFE, 0xEFFFF, 0xFFFFE, 0xFFFFF, 0x10FFFE, 0x10FFFF, ], # noncharacters
+  (0xE000..0xF8FF), # Private use characters
+  (0xF0000..0xFFFFD), # Private use characters
+  (0x100000..0x10FFFD), # Private use characters
+]
+
+def gen_fake_unicode
+  loop do
+    # Generate a random index to select a range
+    c = rand(32..1114111)
+
+    unless @all_non_printable_characters.find { |r| r.include?(c) }
+      return c
+    end
+  end
+end
+
 # Generate a fake value for a field
 def gen_fake_field_val(type_map, field)
   case field.type
   when "bool"
-    rand() < 0.5
+    rand < 0.5
+  when "bytes"
+    Array.new(255).map { rand(255).chr("BINARY") }.join
   when "string"
     # TODO: better random strings with variable lengths
-    "foobar" + '_foo' * rand(0..8)
-  when "int32", "int64", "uint64", "bool", "sint32", "sint64", "uint32"
+    Array.new(255).map { gen_fake_unicode }.join
+  when "uint32"
     # TODO: we may want a normal or poisson distribution?
     # We don't care about negative integers for this benchmark (rarely used)
-    rand(0..300)
-  when "float", "double"
-    rand() * 100.0
+    rand(0..(2**32 - 1))
+  when "uint64"
+    rand(0..(2**64 - 1))
+  when "int32", "sint32"
+    rand(-(2**31 - 1)..(2**31 - 1))
+  when "int64", "sint64"
+    rand(-(2**63 - 1)..(2**63 - 1))
+  when "float" # float32
+    rand.round(7)
+  when "double" # float64
+    rand
   else
     gen_fake_data(type_map, field.type)
   end
@@ -55,7 +86,7 @@ def gen_fake_msg(type_map, msg_def)
       repeated_field.replace(arr)
     elsif field.optional?
       # If optional, randomly set the field or not
-      if rand() < 0.5
+      if rand < 0.5
         field_val = gen_fake_field_val(type_map, field)
         msg.send("#{field.name}=", field_val)
       end
@@ -79,9 +110,9 @@ def gen_fake_data(type_map, type_name)
   type_def = type_map[type_name]
   raise "type not found #{type_name}" unless type_def
 
-  if type_def.instance_of? ProtoBoeuf::Message
+  if type_def.instance_of?(ProtoBoeuf::Message)
     return gen_fake_msg(type_map, type_def)
-  elsif type_def.instance_of? ProtoBoeuf::Enum
+  elsif type_def.instance_of?(ProtoBoeuf::Enum)
     return gen_fake_enum(type_def)
   end
 
@@ -90,18 +121,17 @@ end
 
 # Generate a function to read all fields on every node type
 def gen_walk_fn(type_def)
-
   out = "def walk_#{type_def.name}(node)\n"
   out += "  return unless node\n"
 
-  if type_def.instance_of? ProtoBoeuf::Message
+  if type_def.instance_of?(ProtoBoeuf::Message)
     # For each field of this message
     type_def.fields.each do |field|
       if field.repeated?
         out += "  node.#{field.name}.each { |v| walk_#{field.type}(v) }\n"
       else
         case field.type
-        when "bool", "string", "int32", "int64", "uint64", "bool", "sint32", "sint64", "uint32", "float", "double"
+        when "bool", "string", "bytes", "int32", "sint64", "uint32", "int64", "sint32", "uint64", "float", "double"
           out += "  node.#{field.name}\n"
         else
           out += "  walk_#{field.type}(node.#{field.name})\n"
@@ -109,20 +139,20 @@ def gen_walk_fn(type_def)
       end
     end
 
-  elsif type_def.instance_of? ProtoBoeuf::Enum
+  elsif type_def.instance_of?(ProtoBoeuf::Enum)
     # Do nothing
   end
 
   out += "end"
 
-  #puts out
+  # puts out
 
   # Define the function
   eval(out)
 end
 
 # Parse the proto file so we can generate fake data
-unit = ProtoBoeuf.parse_file "bench/fixtures/benchmark.proto"
+unit = ProtoBoeuf.parse_file("bench/fixtures/benchmark.proto")
 
 # NOTE: for the benchmark, we can guarantee that we don't have overlapping
 # type names, so we can keep a hash of name => definition
@@ -133,7 +163,7 @@ pop_type_map(type_map, unit)
 type_map.each { |type_name, type_def| gen_walk_fn(type_def) }
 
 # Generate some fake instances of the root message type
-fake_msgs = (0..10).map { |i| gen_fake_data(type_map, 'ParkingLot') }
+fake_msgs = (0..10).map { |i| gen_fake_data(type_map, "ParkingLot") }
 
 encoded_bins = fake_msgs.map { |msg| Upstream::ParkingLot.encode(msg).freeze }
 
@@ -141,14 +171,18 @@ bin_sizes = encoded_bins.map { |bin| bin.size }
 total_bin_size = bin_sizes.sum
 puts "total encoded size: #{total_bin_size} bytes"
 
-Benchmark.ips { |x|
-  x.report("decode upstream")  { encoded_bins.each { |bin| Upstream::ParkingLot.decode bin } }
-  x.report("decode protoboeuf") { encoded_bins.each { |bin| ProtoBoeuf::ParkingLot.decode bin } }
+Benchmark.ips do |x|
+  x.report("decode upstream") { encoded_bins.each { |bin| Upstream::ParkingLot.decode(bin) } }
+  x.report("decode protoboeuf") { encoded_bins.each { |bin| ProtoBoeuf::ParkingLot.decode(bin) } }
   x.compare!
-}
+end
 
-Benchmark.ips { |x|
-  x.report("decode and read upstream")  { encoded_bins.each { |bin| walk_ParkingLot(Upstream::ParkingLot.decode bin) } }
-  x.report("decode and read protoboeuf") { encoded_bins.each { |bin| walk_ParkingLot(ProtoBoeuf::ParkingLot.decode bin) } }
+Benchmark.ips do |x|
+  x.report("decode and read upstream") { encoded_bins.each { |bin| walk_ParkingLot(Upstream::ParkingLot.decode(bin)) } }
+  x.report("decode and read protoboeuf") do
+    encoded_bins.each do |bin|
+      walk_ParkingLot(ProtoBoeuf::ParkingLot.decode(bin))
+    end
+  end
   x.compare!
-}
+end
