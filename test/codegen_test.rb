1394ba124f0503e6c2f4de14e365c78af1fd8493
Dave Benvenuti <dave.benvenuti@shopify.com>
2025-01-22 17:46:52 -0800

    WIP simplify

--- test/codegen_test.rb
+++ test/codegen_test.rb
@@ -818,63 +818,39 @@ module ProtoBoeuf
     end
 
     class ProtocInsertionPointVisitor < SyntaxTree::Visitor
-      include SyntaxTree::WithScope
-
       attr_reader :protoc_insertion_points
 
       def initialize
         super
 
         @protoc_insertion_points = []
+        @parents = {}
       end
 
       def visit(node)
-        unless node.nil?
-          previous_child = nil
-
-          node.child_nodes.each do |child|
-            @parents[child] = node
-            @previous_siblings[child] = previous_child
-            @next_siblings[previous_child] = child if previous_child
-            previous_child = child
-          end
-        end
+        node&.child_nodes&.each { |child| @parents[child] = node }
 
         super
       end
+
+      IGNORE_PARENT_TYPES = [SyntaxTree::Program, SyntaxTree::Statements]
 
       visit_methods do
         def visit_comment(node)
           return unless node.value.match(/@@protoc_insertion_point/)
 
-          @protoc_insertion_points << [node]
+          require "debug"
+
+          hierarchy = []
+          parent = @parents[node]
+          while parent
+            hierarchy.unshift(parent) unless IGNORE_PARENT_TYPES.include?(parent.class)
+            parent = @parents[parent]
+          end
+          hierarchy << node
+
+          @protoc_insertion_points << hierarchy
         end
-      end
-
-      DEFAULT_IGNORE_PARENT_NODE_TYPES = [SyntaxTree::BodyStmt, SyntaxTree::Statements, SyntaxTree::Program].freeze
-      # The literal parent may not always be useful.  SyntaxTree::BodyStmt or SyntaxTree::Statements aren't helpful if
-      # we only care about what method or class the given node is a child of.
-      def find_parent(node, ignore_types: DEFAULT_IGNORE_PARENT_NODE_TYPES)
-        parent_candidate = @parents[node]
-
-        if ignore_types.include?(parent_candidate.class)
-          find_parent(parent_candidate)
-        else
-          parent_candidate
-        end
-      end
-
-      def previous_sibling(node)
-        @previous_siblings[node]
-      end
-
-      def next_sibling(node)
-        @next_siblings[node]
-      end
-
-      # Override #inspect so we don't dump the entire SyntaxTree multiple times in the debugger (because of @parents).
-      def inspect
-        "<#{self.class.name} @insertion_points=#{@insertion_points.inspect}>"
       end
     end
 
@@ -901,47 +877,10 @@ module ProtoBoeuf
       parsed_ruby = SyntaxTree.parse(ruby)
       visitor = ProtocInsertionPointVisitor.new.tap { |v| v.visit(parsed_ruby) }
 
-      insertion_points = visitor.protoc_insertion_points.sort_by { |node| node.location.start_line }
+      insertion_points = visitor.protoc_insertion_points.sort_by { |hierarchy| hierarchy.first.location.start_line }
 
-      assert_equal(12, insertion_points.size)
-
-      assert_equal("# @@protoc_insertion_point(requires)", insertion_points[0].value)
-      assert_nil(visitor.find_parent(insertion_points[0]))
-      assert_equal("require", visitor.previous_sibling(insertion_points[0]).message.value)
-
-      assert_equal("# @@protoc_insertion_point(lookup)", insertion_points[1].value)
-      parent = visitor.find_parent(insertion_points[1])
-      assert_syntax_tree_node(SyntaxTree::DefNode, "lookup", parent)
-      grandparent = visitor.find_parent(parent)
-      assert_syntax_tree_node(SyntaxTree::SClass, nil, grandparent)
-      great_grandparent = visitor.find_parent(grandparent)
-      assert_syntax_tree_node(SyntaxTree::ModuleDeclaration, "Color", great_grandparent)
-
-      assert_equal("# @@protoc_insertion_point(resolve)", insertion_points[2].value)
-      parent = visitor.find_parent(insertion_points[1])
-      assert_syntax_tree_node(SyntaxTree::DefNode, "resolve", parent)
-      grandparent = visitor.find_parent(parent)
-      assert_syntax_tree_node(SyntaxTree::SClass, nil, grandparent)
-      great_grandparent = visitor.find_parent(grandparent)
-      assert_syntax_tree_node(SyntaxTree::ModuleDeclaration, "Color", great_grandparent)
-
-      assert_equal("# @@protoc_insertion_point(class_definitions)", insertion_points[3].value)
-      assert_syntax_tree_node(SyntaxTree::VoidStmt, nil, visitor.previous_sibling(insertion_points[3]))
-      parent = visitor.find_parent(insertion_points[3])
-      assert_syntax_tree_node(SyntaxTree::ClassDeclaration, "Vehicle", parent)
-    end
-
-    private
-
-    def assert_syntax_tree_node(klass, identifier, node)
-      assert_kind_of(klass, node)
-
-      case node
-      when SyntaxTree::DefNode
-        assert_equal(identifier, node.name.value)
-      when SyntaxTree::ModuleDeclaration, SyntaxTree::ClassDeclaration
-        assert_equal(identifier, node.constant.constant.value)
-      end
+      # TODO
+      # assert_equal(13, insertion_points.size)
     end
   end
 end
